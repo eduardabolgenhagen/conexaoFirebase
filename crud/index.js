d899e0daa184f0826b3e9a88335c1fbaa1def4f7
eduardabolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2022-06-20 21:15:04 -0300

    init

--- crud/index.js
+++ crud/index.js
@@ -1,15 +1,23 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, doc, setDoc, addDoc } = require('firebase/firestore/lite');
 
-initializeApp({
-    credential: applicationDefault()
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyDFkiQJcGI2omdv7LEbAE9fPLe4-XhOdwo",
+    authDomain: "first-database-access-firebase.firebaseapp.com",
+    projectId: "first-database-access-firebase",
+    storageBucket: "first-database-access-firebase.appspot.com",
+    messagingSenderId: "451994052578",
+    appId: "1:451994052578:web:fa4fb702ac7fbdc0233bbb",
+    measurementId: "G-PY39H8YX90"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
 async function salvar(nomeTabela, id, dado) {
     if (id) {
-        const referenceEntity = await db.collection(nomeTabela).doc(id).set(dado);
+        const referenceEntity = await setDoc(doc(db, nomeTabela, id), dado);
         const savedData = {
             ...dado,
             id: id
@@ -17,7 +25,7 @@ async function salvar(nomeTabela, id, dado) {
         return savedData
 
     } else {
-        const referenceEntity = await db.collection(nomeTabela).add(dado);
+        const referenceEntity = await addDoc(collection(db, nomeTabela), dado);
         const savedData = {
             ...dado,
             id: referenceEntity.id
