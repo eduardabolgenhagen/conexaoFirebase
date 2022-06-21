141565f9eb0b096c0267baed25e53d862c4508a5
eduardabolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2022-06-20 21:47:07 -0300

    init

--- crud/index.js
+++ crud/index.js
@@ -1,5 +1,16 @@
 const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, doc, setDoc, addDoc } = require('firebase/firestore/lite');
+const {
+    getFirestore,
+    collection,
+    doc,
+    setDoc,
+    addDoc,
+    query,
+    where,
+    getDocs,
+    getDoc,
+    deleteDoc
+} = require('firebase/firestore/lite');
 
 const firebaseConfig = {
     apiKey: "AIzaSyDFkiQJcGI2omdv7LEbAE9fPLe4-XhOdwo",
@@ -34,6 +45,47 @@ async function salvar(nomeTabela, id, dado) {
     }
 }
 
-module.exports = {
-    salvar
+async function get(nomeTabela) {
+    const tabelaRef = collection(db, nomeTabela);
+
+    const q = query(tabelaRef);
+
+    const querySnapshot = await getDocs(q);
+
+    const lista = [];
+
+
+    querySnapshot.forEach((doc) => {
+        const data = {
+            ...doc.data(),
+            id: doc.id
+        }
+        lista.push(data);
+    })
+    return lista;
 }
+
+async function getById(nomeTabela, id){
+    const docRef = doc(db, nomeTabela, id);
+    const docSnap = await getDoc(docRef);
+    
+    if (docSnap.exists()) {
+        return docSnap.data();
+      } else {
+        return new Erros("Not foud!");
+      }
+}
+
+async function remove(){
+    const dado = await deleteDoc(doc(db, nomeTabela, id));
+    return {
+        message
+    }
+}
+
+module.exports = {
+    salvar,
+    get,
+    getById
+}
+
--- sandBox.js
+++ sandBox.js
@@ -1,3 +1,19 @@
 const crud = require("./crud");
 
-crud.salvar("pessoas", undefined, {nome: "Eduarda", sobrenome : "Bolgenhagen"});
+// async function salvarDado(){
+//     const savedData = await crud.salvar("pessoas", undefined, { nome: "Eduarda", sobrenome: "Bolgenhagen", idade: 18 })
+//     console.log(savedData);
+// }
+
+// salvarDado();
+
+async function buscardados(){
+    const dados = await crud.get("pessoas");
+    console.log(dados);
+}
+
+async function buscarDadosByID(){
+    const dados = await crud.getById("pessoas", "")
+}
+
+buscardados();
